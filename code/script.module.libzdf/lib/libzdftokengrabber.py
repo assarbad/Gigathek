e7e3251f4451ca1274619e1b4fb4358181a24dbf
68000a <68000a@gmx.net>
2025-03-24 15:56:59 +0100

    ZDF-Tokengrabber angepasst

--- code/script.module.libzdf/lib/libzdftokengrabber.py
+++ code/script.module.libzdf/lib/libzdftokengrabber.py
@@ -4,9 +4,10 @@ import libmediathek3 as libMediathek
 
 def grepToken():
 	response = libMediathek.getUrl('https://www.zdf.de/live-tv')
-	tokenMenu = re.compile("apiToken: '(.+?)'", re.DOTALL).findall(response)[0]
-	tokenPlayer = re.compile('"apiToken": "(.+?)"', re.DOTALL).findall(response)[0]
-	
+	tokenMenu_list = re.compile('\\\\"appToken\\\\":{\\\\"apiToken\\\\":\\\\"(.+?)\\\\"', re.DOTALL).findall(response)
+	tokenMenu = tokenMenu_list[0] if tokenMenu_list else '' 
+	tokenPlayer_list = re.compile('\\\\"videoToken\\\\":{\\\\"apiToken\\\\":\\\\"(.+?)\\\\"', re.DOTALL).findall(response)
+	tokenPlayer = tokenPlayer_list[0] if tokenPlayer_list else ''
 	libMediathek.f_mkdir(libMediathek.pathUserdata(''))
 	libMediathek.f_write(libMediathek.pathUserdata('tokenMenu'), tokenMenu)
 	libMediathek.f_write(libMediathek.pathUserdata('tokenPlayer'), tokenPlayer)
