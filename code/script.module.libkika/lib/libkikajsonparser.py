99a9ff9d9af10126ef15873a41afa3008589eeae
68000a <68000a@gmx.net>
2026-04-23 11:26:09 +0200

    libkika 2.0.10 Behoben: "Sendungen Neu" machte ab und an Probleme

--- code/script.module.libkika/lib/libkikajsonparser.py
+++ code/script.module.libkika/lib/libkikajsonparser.py
@@ -3,7 +3,18 @@ import sys
 import json
 import libmediathek3 as libMediathek
 
+if sys.version_info[0] < 3: # for Python 2
+	pass
+else: # for Python 3
+	from functools import reduce
+
+
 baseUrl = 'https://www.kika.de/api/v1/kikaplayer/kikaapp'
+
+
+def deep_get(dictionary, keys, default=None):
+	return reduce(lambda d, key: d.get(key, default) if isinstance(d, dict) else default, keys.split('.'), dictionary)
+
 
 def getChannel(cid='1'):
 	response = libMediathek.getUrl(baseUrl + '/api/channels/' + cid)
@@ -14,6 +25,7 @@ def getChannel(cid='1'):
 		l.append(d)
 	return l
 
+
 def getBrands():#Shows
 	response = libMediathek.getUrl(baseUrl+'/api/brands?limit=400&orderBy=title&orderDirection=ASC&showEmptyBrands=false')
 	j = json.loads(response)
@@ -23,13 +35,14 @@ def getBrands():#Shows
 		d['name'] = entry['title']
 		if 'description' in entry:
 			d['plot'] = entry['description']
-		d['thumb'] = entry.get('mediumLogoImageUrl', entry.get('smallLogoImageUrl', None))
-		d['_fanart'] = entry.get('smallBackgroundImageUrl', entry.get('mediumLogoImageUrl', None))
+		d['thumb'] = entry.get('mediumLogoImageUrl', entry.get('smallLogoImageUrl', entry.get('largeLogoImageUrl', None)))
+		d['_fanart'] = entry.get('mediumBackgroundImageUrl', entry.get('smallBackgroundImageUrl', entry.get('largeBackgroundImageUrl', None)))
 		d['uri'] = entry['_links']['videos']['href']
 		d['mode'] = 'libKikaListVideos'
 		d['_type'] = 'dir'
 		l.append(d)
 	return l
+
 
 def getVideos(uri='/api/videos?limit=20&orderBy=date&orderDirection=DESC'):
 	response = libMediathek.getUrl(baseUrl+uri)
@@ -38,15 +51,14 @@ def getVideos(uri='/api/videos?limit=20&orderBy=date&orderDirection=DESC'):
 	for entry in j['_embedded']['items']:
 		d = {}
 		d['name'] = entry['title']
-		embedded = entry.get('_embedded',None)
-		if embedded:
-			brand = embedded.get('brand',None)
-			if brand:
-				d['name'] = brand['title'] + ' - ' + d['name']
-		d['plot'] = entry['description']
+		brand = deep_get(entry, '_embedded.brand')
+		if brand:
+			d['name'] = brand['title'] + ' - ' + d['name']
+			d['_fanart'] = brand.get('mediumBackgroundImageUrl', brand.get('smallBackgroundImageUrl', brand.get('largeBackgroundImageUrl', None)))
+		if 'description' in entry:
+			d['plot'] = entry['description']
 		d['_duration'] = str(entry['duration'])
-		d['thumb'] = entry['largeTeaserImageUrl']
-		d['_fanart'] = entry['_embedded']['brand']['smallBackgroundImageUrl']
+		d['thumb'] = entry.get('mediumTeaserImageUrl', entry.get('smallTeaserImageUrl', entry.get('largeTeaserImageUrl', None)))
 		d['_channel'] = entry['broadcaster']
 		#d['_airedISO8601'] = entry['appearDate']
 		d['uri'] = entry['_links']['player-assets']['href']
@@ -54,6 +66,7 @@ def getVideos(uri='/api/videos?limit=20&orderBy=date&orderDirection=DESC'):
 		d['_type'] = 'video'
 		l.append(d)
 	return l
+
 
 def getVideoUrl(uri):
 	mediaHLS = []
